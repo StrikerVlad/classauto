f1e76fada389d2c1788d018f338e340a944a9de7
Владислав Труфанов <@local.itstep>
2019-03-04 17:53:17 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp3/auto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class auto
+    {
+        protected string model { get; set; }
+        protected string brand { get; set; }
+        protected double MaxSpeed { get; set; }
+        protected double Razgondo100 { get; set; }
+        protected int year { get; set; }
+
+
+        static readonly string Wincode;
+
+        public double GetMaxSpeed()
+        {
+            return this.MaxSpeed;
+        }
+
+        public void RachetGar(int year)
+        {
+            if (year < 2000)
+            {
+                Console.WriteLine("Гарантия не действует");
+            }
+
+            else if (year > 2000 && year < 2010)
+            {
+                Console.WriteLine("Гарантия по решению салона");
+            }
+
+            else
+            {
+                Console.WriteLine("Полное покрытие гарантии");
+            }
+        }
+
+        public auto(string model, string brand, int year)
+        {
+            this.model = model;
+            this.brand = brand;
+            this.year = year;
+        }
+
+        public auto()
+        {
+            this.model = model;
+            this.brand = brand;
+            this.year = year;
+        }
+
+             
+    }
+}
